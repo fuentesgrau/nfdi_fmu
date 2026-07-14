8e025f8303b9c5c070e531b09cab95d50b7da5f3
Laura Fuentes Grau <fuentesgrau@proton.me>
2026-07-14 14:51:15 +0200

    Current test bash script

--- test.sh
+++ test.sh
@@ -3,7 +3,6 @@ url_fmu_dostep="http://localhost:8080/api/v2/gateway/fmu/FMU_server/FMI_service/
 
 for run in $(seq 1 100)
 do
-    curl $url_fmu_dostep -s -o /dev/nul
-    curl $url_fmu_getdata -s -o /dev/null -X PUT
+    curl $url_fmu_dostep -s -o /dev/nul -X PUT
 done
 
